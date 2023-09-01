edadf1f6109506d2772688eaa219fd1fec497e32
Techsalam <tajudeenoladiran@gmail.com>
2023-09-01 07:34:23 -0700

    first assigment

---
+++ Salam.py
@@ -0,0 +1,24 @@
+"""Author/Owner:SALAMI"""
+
+#This is a code that calculate area of a circle
+
+print("=====cal area of circle======")
+radius=int(input("Enter Radius:"))
+ac=(radius**2)*3.14
+print(f'Answer:{ac}')
+
+#This a code that calculate simple interest
+
+print("=========cal simple interest======")
+princpal=int(input("Enter Principal:"))
+rate=int(input("Enter Rate:"))
+time=int(input("Enter Time:"))
+si=principal*rate*time//100
+print(f*Answer:{si}")
+
+#This a code that calculate area triangle
+print("========cal area of trianglE=======")
+base=int(input("Enter Base:"))
+height=int(input("Enter Height:"))
+a=0.5*base*heght
+print(f"Answer:{a}")
